7681b557af9254ba609842096ff20c858d729201
GivPenjinun <penjinun.watt@gmail.com>
2023-08-02 22:24:34 +0700

    feat: add portfolio page

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,25 @@
+const portfolios = [
+  {
+    id: 1,
+    name: "Web Design 1",
+    image: "https://cdn.pic.in.th/file/picinth/portfolio1.jpeg",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque pariatur quod maxime possimus. ",
+  },
+  {
+    id: 2,
+    name: "Web Design 2",
+    image: "https://cdn.pic.in.th/file/picinth/portfolio2.jpeg",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque pariatur quod maxime possimus. ",
+  },
+  {
+    id: 3,
+    name: "Web Design 3",
+    image: "https://cdn.pic.in.th/file/picinth/portfolio3.jpeg",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque pariatur quod maxime possimus. ",
+  },
+];
+
+export default portfolios;
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,14 +1,38 @@
 import { useState, useEffect } from "react";
 import "./Portfolio.css";
 import { Link, useNavigate } from "react-router-dom";
-import { FaLaptopCode } from "react-icons/fa";
-
-import img1 from "../assets/img1.png";
+import { LuExternalLink } from "react-icons/lu";
+import portfolios from "./Portfolio.js";
+import portfolio1 from "/src/assets/portfolio1.jpg";
+import portfolio2 from "/src/assets/portfolio1.jpg";
+import portfolio3 from "/src/assets/portfolio1.jpg";
 
 function Portfolio() {
   return (
     <>
-      <section className="Portfolio" id="portfolio"></section>
+      <section className="portfolio" id="portfolio">
+        <h2 className="heading">
+          Latest<span> Portfolio</span>
+        </h2>
+        <div className="portfolio-container">
+          {portfolios.map((item, index) => {
+            return (
+              <>
+                <div className="portfolio-box">
+                  <img src={item.image} alt="portfolio" />
+                  <div className="portfolio-layer">
+                    <h4>{item.name}</h4>
+                    <p>{item.detail}</p>
+                    <a href="#">
+                      <LuExternalLink id="icon" />
+                    </a>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </section>
     </>
   );
 }
