ebd9cee5e63964e27ef1b553ac6faa315e48e659
GivPenjinun <penjinun.watt@gmail.com>
2023-10-27 21:30:44 +0700

    fix: fix image error

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,21 +2,21 @@ const portfolios = [
   {
     id: 1,
     name: "Web Design 1",
-    image: "https://cdn.pic.in.th/file/picinth/portfolio1.jpeg",
+    image: "https://drive.google.com/uc?id=10yP_tvZK12lN5d7_DBIXokvfhMBYEh11",
     detail:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque pariatur quod maxime possimus. ",
   },
   {
     id: 2,
     name: "Web Design 2",
-    image: "https://cdn.pic.in.th/file/picinth/portfolio2.jpeg",
+    image: "https://drive.google.com/uc?id=1NWE8CIL8Z5evCJBGPRByPN5FouAOxIDm",
     detail:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque pariatur quod maxime possimus. ",
   },
   {
     id: 3,
     name: "Web Design 3",
-    image: "https://cdn.pic.in.th/file/picinth/portfolio3.jpeg",
+    image: "https://drive.google.com/uc?id=19roklACVNcdnfpIZNV32R99N-eITNg3Z",
     detail:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque pariatur quod maxime possimus. ",
   },
